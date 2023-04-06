e018cabe9b10314209e646e1328380bec6348020
lakindu62 <lakindu62@gmail.com>
2023-04-06 14:10:17 +0530

    added a ./ infront of my image src link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,7 +9,7 @@ export default function Footer(){
     return(
         <div className="flex justify-center pt-10 items-center">
         <span> Source Code :  </span>
-        <a href="https://github.com/lakindu62/meme-generator"><img className="h-16 w-16" src="src/assets/github.png" /></a> 
+        <a href="https://github.com/lakindu62/meme-generator"><img className="h-16 w-16" src="./src/assets/github.png" /></a> 
         </div>
     )
 
